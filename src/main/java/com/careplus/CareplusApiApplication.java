a86d05a1b24f862f51e941aaf4ad17562beac2f3
Ashwin Bhanage <bhanageashwin28@gmail.com>
2026-03-09 16:47:03 +0530

    initial: spring boot project setup

---
+++ src/main/java/com/careplus/CareplusApiApplication.java
@@ -0,0 +1,13 @@
+package com.careplus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CareplusApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CareplusApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/careplus/careplus_api/CareplusApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.careplus.careplus_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CareplusApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
